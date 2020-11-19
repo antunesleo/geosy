5615b78a98df040f9a53cd7a89b44a497e37e50d
leonardo <antunesleo4@gmail.com>
2020-11-19 14:38:43 -0300

    implements tests for wkt polygon class

--- tests/test_geotypes.py
+++ tests/test_geotypes.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from geosy.geotypes import Wkt
+from geosy.geotypes import Wkt, WktPolygon
 from geosy.exceptions import CorruptedGeometryError
 from geosy.geotypes import GeoJsonPolygon
 
@@ -28,12 +28,32 @@ class TestWkt(TestCase):
         self.assertFalse(invalid_wkt.is_valid)
 
 
-# TODO: Implement these tests
-class TestPolygonWkt(TestCase):
-    pass
+class TestWktPolygon(TestCase):
+
+    def test_init_should_raise_corrupted_geometry_when_geometry_is_corrupted(self):
+        corrupted_wkt_polygon = wkt_dataset.POLYGON_CORRUPTED_BY_COMMA_BETWEEN_COORDINATES
+        with self.assertRaises(CorruptedGeometryError):
+            Wkt(corrupted_wkt_polygon)
+
+    def test_should_have_as_str(self):
+        wkt_polygon = WktPolygon(wkt_dataset.POLYGON)
+        self.assertEqual(wkt_dataset.POLYGON, wkt_polygon.as_str)
+        self.assertEqual(wkt_dataset.POLYGON, str(wkt_polygon))
+
+    def test_is_valid_should_return_True_when_is_valid(self):
+        wkt_polygon = WktPolygon(wkt_dataset.POLYGON)
+        self.assertTrue(wkt_polygon.is_valid)
+
+    def test_is_valid_should_return_False_when_is_invalid(self):
+        invalid_wkt_polygon = WktPolygon(wkt_dataset.POLYGON_INVALID_WITH_CROSS_VERTICES)
+        self.assertFalse(invalid_wkt_polygon.is_valid)
+
+    def test_init_should_raise_value_error_when_shape_is_not_a_polygon(self):
+        with self.assertRaises(ValueError):
+            WktPolygon(wkt_dataset.POINT)
 
 
-class TestPolygonGeoJson(TestCase):
+class TestGeoJsonPolygon(TestCase):
 
     def test_is_valid_should_return_True_when_is_valid(self):
         polygon_geojson = GeoJsonPolygon(geojson_dataset.POLYGON)
