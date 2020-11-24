f23a0a91b91c59a860488568fa697aec0d38b55b
leonardo <antunesleo4@gmail.com>
2020-11-24 20:34:39 -0300

    implement merge_polygon

--- tests/test_geosy.py
+++ tests/test_geosy.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
+from tests.datasets import wkt_str_dataset
+
+from geosy import merge_polygons
 
 
-class ExampleTest(TestCase):
+class TestMergePolygons(TestCase):
 
-    def test_1_should_b_1(self):
-        self.assertEqual(1, 1)
+    def test_should_merge_wkt_polygons(self):
+        merged_wkt_polygon = merge_polygons((
+            wkt_str_dataset.MERGEABLE_POLYGON_1,
+            wkt_str_dataset.MERGEABLE_POLYGON_2
+        ))
+
+        self.assertEqual(merged_wkt_polygon, wkt_str_dataset.MERGED_POLYGON)
